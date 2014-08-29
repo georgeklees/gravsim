8dc628650ec8a68fe79a7c6eab58033382e8e79d
Marionumber1 <gksharkboy@gmail.com>
2014-08-28 21:11:25 -0400

    Added vector magnitude() function

--- code/physics.py
+++ code/physics.py
@@ -1,3 +1,5 @@
+import math
+
 # 2D vector class
 class Vector2:
     def __init__(self, head=(0,0), direction=(0,0)):
@@ -15,3 +17,5 @@ class Vector2:
 
         direction = (self.direction[0] - other.direction[0], self.direction[1] - other.direction[1])
         return Vector2(self.head, direction)
+    def magnitude(self):
+        return math.sqrt((self.direction[0] ** 2) + (self.direction[1] ** 2))
