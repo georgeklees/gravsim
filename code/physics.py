714ea8cd777d71bae33c65cd689a65d7a6cce473
Marionumber1 <gksharkboy@gmail.com>
2014-08-28 21:48:42 -0400

    Fixing vector terminology

--- code/physics.py
+++ code/physics.py
@@ -2,15 +2,15 @@ import math
 
 # 2D vector class
 class Vector2:
-    def __init__(self, head=(0,0), direction=(0,0)):
-        self.head = head
+    def __init__(self, tail=(0,0), direction=(0,0)):
+        self.tail = tail
         self.direction = direction
     def __add__(self, other):
         if type(other) != Vector2:
             raise Exception("Both operands of vector addition must be vectors")
 
         direction = (self.direction[0] + other.direction[0], self.direction[1] + other.direction[1])
-        return Vector2(self.head, direction)
+        return Vector2(self.tail, direction)
     def __mul__(self, other):
         # Scalar multiplication
         if type(other) == float:
