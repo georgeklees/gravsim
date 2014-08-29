d6e32fc90a0d4e27e4c1d08e2e1ace1c6e19a399
Marionumber1 <gksharkboy@gmail.com>
2014-08-28 21:09:26 -0400

    Fixing vector code to be correct

--- code/physics.py
+++ code/physics.py
@@ -1,15 +1,17 @@
 # 2D vector class
 class Vector2:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
+    def __init__(self, head=(0,0), direction=(0,0)):
+        self.head = head
+        self.direction = direction
     def __add__(self, other):
         if type(other) != Vector2:
             raise Exception("Both operands of vector addition must be vectors")
 
-        return Vector2(self.x + other.x, self.y + other.y)
+        direction = (self.direction[0] + other.direction[0], self.direction[1] + other.direction[1])
+        return Vector2(self.head, direction)
     def __sub__(self, other):
         if type(other) != Vector2:
             raise Exception("Both operands of vector subtraction must be vectors")
 
-        return Vector2(self.x - other.x, self.y - other.y)
+        direction = (self.direction[0] - other.direction[0], self.direction[1] - other.direction[1])
+        return Vector2(self.head, direction)
